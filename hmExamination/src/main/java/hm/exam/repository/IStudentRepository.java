e1b7bbf3626bf51ab3cddc6d6f8cfc971f67a473
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-12-22 01:34:42 +0100

    first push

---
+++ hmExamination/src/main/java/hm/exam/repository/IStudentRepository.java
@@ -0,0 +1,9 @@
+package hm.exam.repository;
+
+import hm.exam.model.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStudentRepository extends CrudRepository<Student, Long> {
+}
