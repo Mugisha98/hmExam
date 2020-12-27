5da050ce0da862a62ef2e3c7c12e35bdb9944f24
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-12-27 03:32:47 +0100

    idea

--- hmExamination/src/main/java/hm/exam/repository/IStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package hm.exam.repository;
-
-import hm.exam.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStudentRepository extends CrudRepository<Student, Long> {
-}
--- hmExamination/src/main/java/hm/exam/repository/ISupervisorRepository.java
+++
@@ -1,9 +0,0 @@
-package hm.exam.repository;
-
-import hm.exam.model.Supervisor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ISupervisorRepository extends CrudRepository<Supervisor, Long> {
-}
