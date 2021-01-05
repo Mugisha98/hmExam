1318f1f9f9cbe6a361d160d333e51de2b3beec8b
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2021-01-05 11:39:53 +0100

    Adjustments

--- hmExamination/src/main/java/hm/exam/MainApplication.java
+++ hmExamination/src/main/java/hm/exam/MainApplication.java
@@ -8,8 +8,6 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-        System.out.println("Wakanda4ever");
-        //Jenkins
     }
 
 }
