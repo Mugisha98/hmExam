da51aef72a29d8de09fa5daf9e0f1384a84434a9
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2021-01-25 13:22:35 +0100

    tekeksame wuhu

--- hmExamination/src/main/java/hm/exam/MainApplication.java
+++ hmExamination/src/main/java/hm/exam/MainApplication.java
@@ -9,6 +9,7 @@ public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
 
+        System.out.println("TekEksam wuhu");
     }
 
 }
